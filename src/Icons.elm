f976b02c49091a642e02d7b0dce2cc747f3e6be8
dizider <jehlicka@mjehlicka.eu>
2022-06-20 11:55:21 +0200

    Icons

---
+++ src/Icons.elm
@@ -0,0 +1,49 @@
+module Icons
+    exposing
+        ( delete
+        , edit2
+        , eye
+        )
+
+import Html exposing (Html)
+import Svg exposing (Svg, svg)
+import Svg.Attributes exposing (..)
+
+
+svgFeatherIcon : String -> List (Svg msg) -> Html msg
+svgFeatherIcon className =
+    svg
+        [ class <| "feather feather-" ++ className
+        , fill "none"
+        , height "24"
+        , stroke "currentColor"
+        , strokeLinecap "round"
+        , strokeLinejoin "round"
+        , strokeWidth "2"
+        , viewBox "0 0 24 24"
+        , width "24"
+        ]
+
+
+delete : Html msg
+delete =
+    svgFeatherIcon "delete"
+        [ Svg.path [ d "M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z" ] []
+        , Svg.line [ x1 "18", y1 "9", x2 "12", y2 "15" ] []
+        , Svg.line [ x1 "12", y1 "9", x2 "18", y2 "15" ] []
+        ]
+
+
+edit2 : Html msg
+edit2 =
+    svgFeatherIcon "edit-2"
+        [ Svg.path [ d "M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" ] []
+        ]
+
+
+eye : Html msg
+eye =
+    svgFeatherIcon "eye"
+        [ Svg.path [ d "M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" ] []
+        , Svg.circle [ cx "12", cy "12", r "3" ] []
+        ]
